3a9d26c8493a2e22df17deabef42e798df3584b7
SweetHome <103113557+li-peifeng@users.noreply.github.com>
2025-01-05 00:52:15 +0100

    upload dev dist

---
+++ dist/assets/index-86b526a3.js
@@ -0,0 +1,1 @@
+import{h as t,ae as e,a8 as r}from"./index-c275bac5.js";import n from"./Upload-e150ecc0.js";import"./index-aedeab25.js";const c=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{c as default};
