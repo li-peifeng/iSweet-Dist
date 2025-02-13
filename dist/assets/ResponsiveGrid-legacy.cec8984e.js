7ae481c58cdb72c5a93230ca6d769360c455b768
Noah Hsu <i@nn.ci>
2025-02-13 00:12:16 +0000

    upload 3.42.0 dist files

---
+++ dist/assets/ResponsiveGrid-legacy.cec8984e.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.c543e72c.js"],(function(e){"use strict";var t,n,r;return{setters:[function(e){t=e.V,n=e.h,r=e.da}],execute:function(){e("R",(function(e){var i;return"responsive"===t("settings_layout")&&(i="repeat(auto-fill, minmax(424px, 1fr))"),n(r,{w:"$full",gap:"$2",templateColumns:{"@initial":"1fr","@lg":i},get children(){return e.children}})}))}}}));
